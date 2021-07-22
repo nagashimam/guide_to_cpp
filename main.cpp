27722b9d38bd09192af2d9b0860769d0436296a2
masato <masato.ngsm@gmail.com>
2021-07-22 12:42:59 +0000

    文字列

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "one"s << "two"s << "three" << std::endl;
+    std::cout << "one"s << "two"s << "three"s << std::endl;
+    std::cout << "one"s + "two"s + "three"s << std::endl;
+    std::cout << "one\n"s << "two\n"s << "three\n"s << std::endl;
 }
