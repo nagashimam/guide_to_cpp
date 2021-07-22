6b6284c7b6ae7456fc9067d66b6b68418745d943
masato <masato.ngsm@gmail.com>
2021-07-22 12:50:59 +0000

    整数と浮動小数点数

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
 int main(){
-    std::cout << "one"s << "two"s << "three"s << std::endl;
-    std::cout << "one"s + "two"s + "three"s << std::endl;
-    std::cout << "one\n"s << "two\n"s << "three\n"s << std::endl;
+    std::cout << 2 + "30" << std::endl;
 }
